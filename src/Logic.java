3beb7432e36f46bfed1690ed8e0601b4ce975eea
Giulia F. Telli <gt1656@pleasantonusd.net>
2022-04-25 10:46:44 -0700

    rudementary guess checker, does not work for words with two letters next to each other/possibly does not work with all two letters

--- src/Logic.java
+++ src/Logic.java
@@ -8,14 +8,35 @@ public class Logic {
         Scanner input = new Scanner(System.in);
         int g = 6;
         String guess = "";
-        while (guess != word) {
-            // guess = input.nextLine();
-            // if (guess.length!=5){
-            //     System.out.println("Must be 5 Chars");
-            // }
-            // for (int i = 0){
+        System.out.println("Enter your first guess!");
+        while (guess != word){
+            guess = input.nextLine(); //works as intended
+            if (guess.length()!=5){
+                System.out.println("Must be 5 letters!");
 
-            // }
+            }
+           for (int i = 0; i < guess.length(); i++){
+               for(int j = 0; j <word.length(); j++){
+                   if(guess.substring(i, i+1).equals(word.substring(j, j+1))){
+                       if(j == i){
+                        System.out.println("The guess " + guess.substring(i, i+1) + " is correct and in the right spot");
+                        //temp code to be replaced with GUI 
+                        //does not work for double letters
+                       }
+                       else{
+                       System.out.println("The guess " + guess.substring(i, i+1) + " is correct, but in the wrong spot");
+                       //temp code to be replaced with GUI
+                       //does not work for double letters
+                       }
+                       
+                   }
+               }
+
+           }
+            
         }
     }
 }
+
+//this is possibly not an issue but the terminal needs to be erased before every reboot 
+//probbaly is an issue
