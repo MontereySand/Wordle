ad9592c6847e1fb8a03198bbcd113a0c5dbbf330
Aditya Maramreddy <99278170+MontereySand@users.noreply.github.com>
2022-04-30 20:47:24 -0700

    Update Logic.java
    Hey Guilia, I just need a little description on what line 8 does exactly. I set my issues into a comment

--- src/Logic.java
+++ src/Logic.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Logic {
     public static void main(String[] args) {
 
-        String[] words = { "hello", "broke", "money", "nigel", "super" };
-        String word = words[(int) (Math.random() * words.length)];
+        String[] words = { "hello", "broke", "money", "nigel", "super" }; // will later tap into the text file and pull from there
+                                                                          // find out how do that...perferebly in a lightweight manner
+        String word = words[(int) (Math.random() * words.length)]; // for Guilia from col 29-65 in this line, all it does is change it to an INT, i'm not too sure how that makes sense
+                                                                   // for Guilia its just makeing x as an int, its making "String word = words[x]" I don't get how this works lmaoo, maybe this is not it's final form
         Scanner input = new Scanner(System.in);
         int g = 6;
         String guess = "";
