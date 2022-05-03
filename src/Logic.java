deccff07892cdefb93cdd886f3f372667c86afc9
Giulia F. Telli <gt1656@pleasantonusd.net>
2022-05-03 10:16:10 -0700

    renamed file; replaced logic.java w/ scanner sys

--- src/Logic.java
+++ src/Logic.java
@@ -3,19 +3,22 @@ import java.util.Scanner;
 public class Logic {
     public static void main(String[] args) {
 
-        String[] words = { "hello", "broke", "money", "nigel", "super" };
-        String word = words[(int) (Math.random() * words.length)];
+        String[] words = { "hello", "broke", "money", "nigel", "super" }; // will later tap into the text file and pull from there
+                                                                          // find out how do that...perferebly in a lightweight manner
+        String word = words[(int) (Math.random() * words.length)];  //gets random int and asks for that int as an index in words[]    
+        System.out.println(word);    
         Scanner input = new Scanner(System.in);
-        int g = 6;
+
         String guess = "";
         System.out.println("Enter your first guess!");
         while (guess != word){
             guess = input.nextLine(); //works as intended
             if (guess.length()!=5){
                 System.out.println("Must be 5 letters!");
+                continue; 
 
             }
-           for (int i = 0; i < guess.length(); i++){
+            for (int i = 0; i < guess.length(); i++){
                for(int j = 0; j <word.length(); j++){
                    if(guess.substring(i, i+1).equals(word.substring(j, j+1))){
                        if(j == i){
@@ -27,12 +30,15 @@ public class Logic {
                        System.out.println("The guess " + guess.substring(i, i+1) + " is correct, but in the wrong spot");
                        //temp code to be replaced with GUI
                        //does not work for double letters
+                       // need a way to check if all of the letters are in the right spot - from Krrish
+                       // need to add which letters are not correct at all - from Krrish
                        }
                        
                    }
-               }
+               
 
            }
+        }
             
         }
     }
---
+++ src/Logic_Scanner.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class Logic_Scanner {
+    public static void main(String[] args) {
+        ArrayList<String> words = new ArrayList<String>();
+        Scanner sc = null; 
+        try {
+            sc = new Scanner(new FileReader("dict.txt"));
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        String str; 
+        // checking end of file
+        while (sc.hasNext()) {
+            str = sc.next(); 
+            // adding each string to arraylist
+            words.add(str.toLowerCase());
+            
+        }
+
+        //(tester) 
+        //System.out.println(words); 
+
+        String word = words.get((int)(Math.random() * words.size()));
+        Scanner input = new Scanner(System.in);
+        int g = 6;
+        String guess = "";
+        System.out.println(word); //tester
+        System.out.println("Enter your first guess!");
+        
+        while (guess != word){
+
+            guess = input.nextLine(); //works as intended
+            if(guess.equals(word)){
+                System.out.println("You got it. Game over. Winner winner chicken dinner");
+                break; 
+            }
+           else if (guess.length()!=5){
+                System.out.println("Must be 5 letters!");
+
+            }
+        else {
+           for (int i = 0; i < guess.length(); i++){
+               for(int j = 0; j <word.length(); j++){
+                   if(guess.substring(i, i+1).equals(word.substring(j, j+1))){
+                    
+                    
+                       if(j == i){
+                        System.out.println("The guess " + guess.substring(i, i+1) + " is correct and in the right spot");
+                        
+                        //temp code to be replaced with GUI 
+                        //does not work for double letters
+                       }
+                       else{
+                       System.out.println("The guess " + guess.substring(i, i+1) + " is correct, but in the wrong spot");
+                       //temp code to be replaced with GUI
+                       //does not work for double letters
+                       
+                       }
+                       
+                   }
+               }
+
+           }
+        }
+            
+        }
+    }
+}
+
+//this is possibly not an issue but the terminal needs to be erased before every reboot 
+//probbaly is an issue
