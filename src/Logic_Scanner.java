de2cd1dd1e52edad914b186fd3a1c9db9f07d46f
Aditya Maramreddy <99278170+MontereySand@users.noreply.github.com>
2022-05-05 13:25:03 -0700

    Update Logic_Scanner.java
    I'm showing gautam how git works

--- src/Logic_Scanner.java
+++ src/Logic_Scanner.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Logic_Scanner {
     public static void main(String[] args) {
         //gameMethod(); 
-        testerMethod("randy"); 
+        testerMethod("randr"); 
         }
 
 
