f7a5a4169ef9babc2cdaf2ca9693effcd66da8d0
Aditya Maramreddy <99278170+MontereySand@users.noreply.github.com>
2022-05-11 09:51:08 -0700

    Beautifying

--- src/Wordle.java
+++ src/Wordle.java
@@ -2,29 +2,24 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 import java.util.ArrayList;
-
 public class Wordle {
     private String gameanswer;
     public Wordle() {
         gameanswer = wordExtractor();
     }
     public String wordExtractor() {
-
         ArrayList<String> words = new ArrayList<String>();
         Scanner sc = null;
-
         try {
             sc = new Scanner(new FileReader("lib/dict.txt"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
         String str;
         while (sc.hasNext()) {
             str = sc.next();
             words.add(str);
         }
-
         String word = words.get((int) (Math.random() * words.size()));
         return word;
     }
@@ -48,12 +43,7 @@ public class Wordle {
     public void play() {
         Wordle wordle = new Wordle();
         String currentGuess;
-        System.out.println(wordle.getAnswer());// just shows the answer
-        // Scanner input = new Scanner(System.in);// temp string
         System.out.println("Enter Guess #1:");
-
-        // System.out.println(wordle.format(input.nextLine()));
-        // input.close();
         Scanner input = new Scanner(System.in);
         currentGuess = input.nextLine();
         while (currentGuess.length() != 5) {
@@ -68,14 +58,12 @@ public class Wordle {
         System.out.println(wordle.format(currentGuess));
         int i = 1;
         while (currentGuess != wordle.getAnswer()) {
-
             System.out.println("Enter Guess #" + (i + 1) + ":");
             currentGuess = input.nextLine();
             if (currentGuess.length() != 5) {
                 System.out.println("Enter A 5 Letter Word, Try again");
                 continue;
             }
-
             System.out.println(wordle.format(currentGuess));
             if (i == 5) {
                 System.out.println("Better luck next time, the answer was");
@@ -87,14 +75,10 @@ public class Wordle {
                 System.out.println("Good Job! You won the Wordle");
                 break;
             }
-            
-
         }
-
     }
     public static void main(String[] args) {
         Wordle wordle = new Wordle();
         wordle.play();
     }
-
 }
