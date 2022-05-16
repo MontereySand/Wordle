2e88012f88fa639888c1f7bf29d0b275c985eb96
Aditya Maramreddy <99278170+MontereySand@users.noreply.github.com>
2022-05-15 23:27:24 -0700

    Removed some useless Java AWT imports

--- src/Graphics.java
+++ src/Graphics.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class Graphics implements ActionListener {
