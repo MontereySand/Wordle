02014f482672b0f42e96e9a61c00a8c6c6141507
Giulia F. Telli <gt1656@pleasantonusd.net>
2022-05-16 18:13:24 -0700

    idk

--- src/Graphics.java
+++ src/Graphics.java
@@ -1,9 +1,29 @@
 import javax.swing.*;
-public class Graphics {
-    private JImage grid;
-
-    Graphics() {
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class Graphics implements ActionListener {
+    private int clicks = 0;
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
+    public Graphics() {
+        frame = new JFrame();
+        JButton button = new JButton("Click me");
+        label = new JLabel("Number of clicks: 0");
+        panel = new JPanel();
+        panel.add(button);
+        panel.add(label);
+        frame.setTitle("Our GUI");
+        frame.pack();
+        frame.setVisible(true);
+        frame.getContentPane().setBackground(new Color(255, 0, 0));
+    }
     public static void main(String[] args) {
-         
+        new Graphics();
+    }
+    public void actionPerformed(ActionEvent e) {
+        clicks++; // increments each time you press button
+        label.setText("Number of clicks:" + clicks);
     }
 }
