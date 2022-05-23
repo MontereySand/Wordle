c166b8120cef30ea2aa181da67a8f5e778e684fe
Giulia F. Telli <gt1656@pleasantonusd.net>
2022-05-23 10:34:27 -0700

    tester, sucks

--- src/Graphics.java
+++ src/Graphics.java
@@ -1,9 +1,8 @@
 import javax.swing.*;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 public class Graphics implements ActionListener {
     private int clicks = 0;
     private JLabel label;
@@ -11,18 +10,31 @@ public class Graphics implements ActionListener {
     private JPanel panel;
     public Graphics() {
         frame = new JFrame();
-        JButton button = new JButton("Click me");
-        label = new JLabel("Number of clicks: 0");
-        panel = new JPanel();
-        panel.add(button);
-        panel.add(label);
-        frame.setTitle("Our GUI");
-        frame.pack();
+        frame.getContentPane().setBackground(new Color(50, 50, 50));
+        ImageIcon icon = new ImageIcon("lib/Grid.png"); 
+        frame.add(new JLabel(icon)); 
+        frame.setPreferredSize(new Dimension(800, 800));
         frame.setVisible(true);
-        frame.getContentPane().setBackground(new Color(255, 0, 0));
+        frame.pack(); 
+        frame.setLocationRelativeTo(panel);
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        
+            JFrame myFrame = new JFrame();
+            JPanel myPanel = new JPanel();
+        
+            for (int i = 0; i < alphabet.length(); i++) {
+                myPanel.add(new JButton(alphabet.substring(i, i + 1)));
+            }
+        
+            myFrame.add(myPanel);
+            myFrame.pack();
+            myFrame.setVisible(true);
+    
     }
+
     public static void main(String[] args) {
         new Graphics();
+        
     }
     public void actionPerformed(ActionEvent e) {
         clicks++; // increments each time you press button
