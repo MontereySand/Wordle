5a49b117b4da58e29b5e06839700e1bba18402a1
Aditya Maramreddy <99278170+MontereySand@users.noreply.github.com>
2022-05-27 08:56:56 -0700

    changing imports

--- src/Graphics.java
+++ src/Graphics.java
@@ -1,37 +1,14 @@
 
 import javax.management.Query;
 import javax.swing.*;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.Window;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
-import javax.swing.BorderFactory;
-import javax.swing.Icon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.border.Border;
-import java.awt.event.*;
 
 public class Graphics implements ActionListener {
     private int clicks = 0;
