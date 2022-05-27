afec8eb4278c3e209a86efd2771edd4cd3c38f72
gt1656 <62352979+gt1656@users.noreply.github.com>
2022-05-27 09:42:47 -0700

    Update Wordle.java

--- src/Wordle.java
+++ src/Wordle.java
@@ -6,15 +6,11 @@ import java.util.TimerTask;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JTextField;
 import java.util.ArrayList;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-
 public class Wordle implements ActionListener {
     private final Timer timer;
-    private final String gameanswer;
+    private final String gameanswer; 
     private final String specialChars;
     private final TimerTask task;
     private int seconds;
@@ -22,37 +18,9 @@ public class Wordle implements ActionListener {
     private JLabel[][] grid;
     private JTextField inputField;
     private JButton button;
-    private JPanel gridPanel;
-    private JPanel inputPanel;
     private String currentGuess;
-    public final int WORD_LENGTH;
     public Wordle() {
         this.currentGuess = "";
-        WORD_LENGTH = 5;
-        frame = new JFrame("Wordle");
-        frame.setLayout(new BorderLayout());
-        grid = new JLabel[6][5];
-        inputField = new JTextField();
-        button = new JButton("Submit");
-        for (int row = 0; row < grid.length; row++) {
-            for (int col = 0; col < grid[0].length; col++) {
-                grid[row][col] = new JLabel(); // creating the cells
-            }
-        }
-        inputPanel = new JPanel();// uses FLowlayout by default
-        inputPanel.add(inputField);
-        inputPanel.add(button);
-        frame.add(inputPanel, BorderLayout.NORTH);
-        gridPanel = new JPanel();
-        gridPanel.setLayout(new GridLayout(6,5));
-        for(int row = 0; row < grid.length; row++){
-            for (int column = 0; column < grid.length; column++) {
-                gridPanel.add(grid[row][column]);
-            }
-        }
-        
-        //frame.add(inputField, BorderLayout.NORTH);
-
         gameanswer = wordExtractor();
         specialChars = "`1234567890-=~!@#$%^&*()_+[]}|;:',./<>?";
         seconds = 0;
@@ -65,7 +33,6 @@ public class Wordle implements ActionListener {
         };
     }
     public String getAnswer() {
-
         return gameanswer;
     }
     public String wordExtractor() {
@@ -87,7 +54,41 @@ public class Wordle implements ActionListener {
     public String format(String s) {
         String formatted = "";
         String answer = gameanswer.toLowerCase();
-        for (int i = 0; i < WORD_LENGTH; i++) {
+        String checker = "";
+        String checker2 = ""; 
+        boolean isTwo = false; 
+        for(int x = 0; x < answer.length(); x++){
+            for(int y = 0; y < answer.length(); y++){
+                if(answer.substring(x, x+1).equals(answer.substring(y, y+1)) && y!=x){
+                    if(isTwo && !answer.substring(y, y+1).equals(checker)){
+                        checker2 = answer.substring(y, y+1); 
+                    }
+                    else{
+                    checker = answer.substring(y, y+1); 
+                    isTwo = true; }
+                }
+            }
+
+        }
+      //  System.out.println(checker); 
+        //System.out.println(checker2); 
+        for(int x = 0; x < answer.length(); x++){
+            for(int y = 0; y < answer.length(); y++){
+                if(s.substring(x, x+1).equals(s.substring(y, y+1)) && y !=x){
+                    if(checker.equals(s.substring(x, x+1)) || checker2.equals(s.substring(x, x+1))){
+                        //System.out.println("i do something"); 
+                        continue; 
+                    }
+                else{
+                    s = s.substring(0, y) + "_" + s.substring(y+1);
+                }
+                   
+                }
+            }
+
+        }
+        System.out.println(s); 
+        for (int i = 0; i < 5; i++) {
             if (s.substring(i, i + 1).equals(answer.substring(i, i + 1))) {
                 formatted += answer.substring(i, i + 1);
             } else if (answer.contains(s.substring(i, i + 1))) {
@@ -97,62 +98,6 @@ public class Wordle implements ActionListener {
             }
         }
         return formatted;
-    }
-    public void play() {
-        Wordle wordle = new Wordle();
-        seconds = 0;
-        String currentGuess;
-        System.out.println("Enter Guess #1:");
-        Scanner input = new Scanner(System.in);
-        currentGuess = this.currentGuess.toLowerCase();
-        while (currentGuess.length() > 5 || currentGuess.length() < 5) {
-            System.out.println("Enter A 5 Letter Word, Try again");
-            System.out.println("Enter Guess #1: ");
-            currentGuess = input.nextLine();
-        }
-        for (int i = 0; i < currentGuess.length(); i++) {
-            while (specialChars.contains(currentGuess.substring(i, i + 1))) {
-                System.out.println("Enter a valid word, try again");
-                System.out.println("Enter Guess #1: ");
-                currentGuess = input.nextLine();
-            }
-        }
-        if (wordle.format(currentGuess).equals(wordle.getAnswer())) {
-            System.out.println("First try? Seems a little suspicious :)");
-            System.out.println("You won in " + seconds + " seconds");
-            return;
-        }
-        System.out.println(wordle.format(currentGuess));
-        int i = 1;
-        while (!currentGuess.equals(wordle.getAnswer())) {
-            System.out.println("Enter Guess #" + (i + 1) + ":");
-            currentGuess = input.nextLine();
-            if (currentGuess.length() != 5) {
-                System.out.println("Enter A 5 Letter Word, Try again");
-                continue;
-            }
-            for (int j = 0; j < currentGuess.length(); j++) {
-                if (specialChars.contains(currentGuess.substring(j, j + 1))) {
-                    System.out.println("Enter a valid word, try again");
-                    System.out.println("Enter Guess #" + (i + 1) + ";");
-
-                    currentGuess = input.nextLine();
-                }
-            }
-            System.out.println(wordle.format(currentGuess));
-            if (wordle.format(currentGuess).equals(wordle.getAnswer())) {
-                System.out.println("Good Job! You won the Wordle");
-                System.out.println("You won in " + seconds + " seconds");
-                break;
-            }
-            if (i > 4) {
-                System.out.println("Better luck next time, the answer was");
-                System.out.println(wordle.getAnswer());
-                System.out.println("You took " + seconds + " seconds");
-                return;
-            }
-            i++;
-        }
     }
     public String loop(String e) {
         String s = format(e);
@@ -168,3 +113,4 @@ public class Wordle implements ActionListener {
         wordle.loop("e");
     }
 }
+
